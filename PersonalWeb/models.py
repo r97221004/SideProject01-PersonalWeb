fcee971ba2f770d3b905bfc4b322e76f5e12d23e
r97221004 <r97221004@ntu.edu.tw>
2021-10-01 17:21:02 +0800

    修改 models.py

--- PersonalWeb/models.py
+++ PersonalWeb/models.py
@@ -7,4 +7,9 @@ class Message(db.Model):
     name = db.Column(db.String(20))
     body = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default = datetime.utcnow, index = True )
+
+
+# utcnow 表示現在的世界協調時間
+# 在這裡不要加括號 
+# index = True 表示在這欄位加索引，這樣在搜索 timestamp 時會變快
     
