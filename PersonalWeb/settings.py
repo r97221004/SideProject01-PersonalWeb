f9a837fb5ceb25b413147fd60b216a90f54de266
r97221004 <r97221004@ntu.edu.tw>
2021-10-02 17:45:07 +0800

    刪除一些 css 與 js 的檔案

--- PersonalWeb/settings.py
+++ PersonalWeb/settings.py
@@ -16,4 +16,4 @@ dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
-DEBUG_TB_INTERCEPT_REDIRECTS=False # DebugToolbar  要用到的設定
+DEBUG_TB_INTERCEPT_REDIRECTS = False # DebugToolbar  要用到的設定
